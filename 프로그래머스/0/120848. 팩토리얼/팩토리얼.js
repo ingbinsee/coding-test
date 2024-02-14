ab58126fd670f0bbabfe9764cc7d93f9deef488e
Lee Eun Bin <ingbinsee@gmail.com>
2024-02-14 23:14:00 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let num = 1;
+    for(let i = 1; i <= n; i++) {
+        num *= i;
+        if(num === n) {
+            return i;
+        } else if(num > n) {
+            return i - 1;
+        }
+    }
+}
